048377003f1a8994f1c72592c67e011ade231172
Orbfer <misha9043@gmail.com>
2024-05-29 01:00:48 +0300

    Finished Project!

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let firstNum;
 let secondNum;
 let operator;
 const display = document.querySelector("#screen");
+const value = document.querySelector("#value-screen");
 const numArr = document.querySelectorAll(".number");
 const oprArr = document.querySelectorAll(".operator");
 const numClear = document.querySelector(".clear");
@@ -10,12 +11,29 @@ const numDecimal = document.querySelector(".decimal");
 const numOperate = document.querySelector(".operate");
 numArr.forEach((button) => {
   button.addEventListener("click", () => {
-    display.textContent += button.innerHTML;
+    if (value.textContent == "") {
+      display.textContent += button.innerHTML;
+    }
   });
 });
 oprArr.forEach((button) => {
   button.addEventListener("click", () => {
-    display.textContent += " " + button.innerHTML + " ";
+    if (
+      display.textContent.includes("x") ||
+      display.textContent.includes("+") ||
+      display.textContent.includes("-") ||
+      display.textContent.includes("÷") ||
+      display.textContent.includes("%")
+    ) {
+      operate();
+    }
+    if (value.textContent == "") {
+      display.textContent += " " + button.innerHTML + " ";
+    } else {
+      display.textContent = value.textContent;
+      value.textContent = "";
+      display.textContent += " " + button.innerHTML + " ";
+    }
   });
 });
 numDelete.addEventListener("click", () => {
@@ -23,23 +41,28 @@ numDelete.addEventListener("click", () => {
     0,
     display.textContent.length - 1
   );
+  value.textContent = "";
 });
 numClear.addEventListener("click", () => {
   display.textContent = "";
+  value.textContent = "";
 });
 numDecimal.addEventListener("click", () => {
-  if (!display.textContent.includes(".")) display.textContent += ".";
+  if (!display.textContent.includes(".")) {
+    display.textContent += ".";
+    value.textContent = "";
+  }
+  let valueArr = display.textContent.split(" ");
+  if ((valueArr.length = 3)) {
+    secondNum = valueArr.at(2);
+    if (!secondNum.includes(".")) {
+      display.textContent += ".";
+      value.textContent = "";
+    }
+  }
 });
 numOperate.addEventListener("click", () => {
-  let valueArr = display.textContent.split(" ");
-  firstNum = parseInt(valueArr.at(0));
-  secondNum = parseInt(valueArr.at(2));
-  operator = valueArr.at(1);
-  if (operator == "+") display.textContent = add(firstNum, secondNum);
-  if (operator == "-") display.textContent = subtract(firstNum, secondNum);
-  if (operator == "x") display.textContent = multiply(firstNum, secondNum);
-  if (operator == "÷") display.textContent = divide(firstNum, secondNum);
-  if (operator == "%") display.textContent = modulus(firstNum, secondNum);
+  operate();
 });
 function add(a, b) {
   return a + b;
@@ -51,8 +74,22 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
+  if (b == 0) {
+    alert("Cant divide by 0!");
+  }
   return a / b;
 }
 function modulus(a, b) {
   return a % b;
+}
+function operate() {
+  let valueArr = display.textContent.split(" ");
+  firstNum = parseInt(valueArr.at(0));
+  secondNum = parseInt(valueArr.at(2));
+  operator = valueArr.at(1);
+  if (operator == "+") value.textContent = add(firstNum, secondNum);
+  if (operator == "-") value.textContent = subtract(firstNum, secondNum);
+  if (operator == "x") value.textContent = multiply(firstNum, secondNum);
+  if (operator == "÷") value.textContent = divide(firstNum, secondNum);
+  if (operator == "%") value.textContent = modulus(firstNum, secondNum);
 }
