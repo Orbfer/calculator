835b4f6008e0f2774b362c8dd170d1a990aa597c
Orbfer <misha9043@gmail.com>
2024-05-28 23:01:13 +0300

    Finished Part 5 and 6 of the project!

--- scripts.js
+++ scripts.js
@@ -1,6 +1,46 @@
 let firstNum;
 let secondNum;
-let Operator;
+let operator;
+const display = document.querySelector("#screen");
+const numArr = document.querySelectorAll(".number");
+const oprArr = document.querySelectorAll(".operator");
+const numClear = document.querySelector(".clear");
+const numDelete = document.querySelector(".delete");
+const numDecimal = document.querySelector(".decimal");
+const numOperate = document.querySelector(".operate");
+numArr.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.textContent += button.innerHTML;
+  });
+});
+oprArr.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.textContent += " " + button.innerHTML + " ";
+  });
+});
+numDelete.addEventListener("click", () => {
+  display.textContent = display.textContent.substring(
+    0,
+    display.textContent.length - 1
+  );
+});
+numClear.addEventListener("click", () => {
+  display.textContent = "";
+});
+numDecimal.addEventListener("click", () => {
+  if (!display.textContent.includes(".")) display.textContent += ".";
+});
+numOperate.addEventListener("click", () => {
+  let valueArr = display.textContent.split(" ");
+  firstNum = parseInt(valueArr.at(0));
+  secondNum = parseInt(valueArr.at(2));
+  operator = valueArr.at(1);
+  if (operator == "+") display.textContent = add(firstNum, secondNum);
+  if (operator == "-") display.textContent = subtract(firstNum, secondNum);
+  if (operator == "x") display.textContent = multiply(firstNum, secondNum);
+  if (operator == "÷") display.textContent = divide(firstNum, secondNum);
+  if (operator == "%") display.textContent = modulus(firstNum, secondNum);
+});
 function add(a, b) {
   return a + b;
 }
@@ -15,10 +55,4 @@ function divide(a, b) {
 }
 function modulus(a, b) {
   return a % b;
-}
-function operate(a, b, c) {
-  if (a == "+") return add(b, c);
-  else if (a == "-") return subtract(b, c);
-  else if (a == "*") return multiply(b, c);
-  else if (a == "/") return divide(b, c);
 }
